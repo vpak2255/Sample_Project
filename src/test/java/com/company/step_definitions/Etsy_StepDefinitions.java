22e3851bc19140cfdadc8458b53957a80c516b35
Veronica Pak <nikampak@gmail.com>
2021-08-26 14:48:49 -0400

    added log function

--- src/test/java/com/company/step_definitions/Etsy_StepDefinitions.java
+++ src/test/java/com/company/step_definitions/Etsy_StepDefinitions.java
@@ -9,9 +9,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Etsy_StepDefinitions {
 
     Login_Etsy login = new Login_Etsy();
